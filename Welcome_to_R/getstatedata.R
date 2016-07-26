d3683eda0957e45dad56b367f0e8d2facb53f5e4
Elin Waring <elin.waring@gmail.com>
2016-07-25 21:03:56 -0400

    Try a different strategy for View().

--- Welcome_to_R/getstatedata.R
+++ Welcome_to_R/getstatedata.R
@@ -4,5 +4,5 @@ data1990 <- getCensus(name="sf3", vintage=1990, key=censuskey,
                       vars=c("P0070001", "P0070002", "P114A001"),
                       region="state:*")
 colnames(data1990) <- col.names
-merge(data1990, state.fips)
-View(data1990)
+data1990 <- merge(data1990, state.fips)
+
