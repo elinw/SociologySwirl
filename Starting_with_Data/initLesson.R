d6fb22389d436f2110d5d4393de581b73c1f9c3e
Elin Waring <elin.waring@gmail.com>
2016-08-01 10:49:47 -0400

    Add first data lesson.

---
+++ Starting_with_Data/initLesson.R
@@ -0,0 +1,7 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+population<-c(1438159, 1636268, 2321580, 2621793, 473279 )
+square.miles<-c(42.47, 22.82, 108.1, 69.5, 57.92)
+
