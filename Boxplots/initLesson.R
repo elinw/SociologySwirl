3da0a147fdfb2ba66efa242e97d54e7e49563974
Elin Waring <elin.waring@gmail.com>
2016-09-18 20:32:20 -0400

    Add box plot lesson

---
+++ Boxplots/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+library(ggplot2)
+carat.5num <- fivenum(diamonds$carat)
+vline3 <- geom_vline(xintercept = carat.5num[3])
+vline4 <- geom_vline(xintercept = carat.5num[4])
+vline5 <- geom_vline(xintercept = carat.5num[5])
