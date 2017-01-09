b9c68c41b745e4fcc126662bac57f10235cb6cbc
Elin Waring <elin.waring@gmail.com>
2017-01-09 06:22:30 -0500

    Add new lesson (Group Means and Variances

---
+++ Group_Means_and_Variances/initLesson.R
@@ -0,0 +1,14 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+chickwts$mean_weight <- mean(chickwts$weight)
+chickwts$weight_deviation <- chickwts$weight - chickwts$mean_weight
+chickwts$median_weight <- median(chickwts$weight)
+chickwts$weight_deviation_median <- chickwts$weight - chickwts$median_weight
+# Create TSS function
+TSS <- function(x) {
+  n <- length(x)
+  TSS <- var(x)*(n-1)
+  TSS
+}
